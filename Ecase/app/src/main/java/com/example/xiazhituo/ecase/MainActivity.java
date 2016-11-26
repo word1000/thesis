596e290d8951818c8fff882f908735020c0b0791
夏知拓 <xiazhituo@xiazhituodeMacBook-Pro.local>
2016-11-26 11:56:53 +0800

    add ble interactive with nrf51822

--- Ecase/app/src/main/java/com/example/xiazhituo/ecase/MainActivity.java
+++ Ecase/app/src/main/java/com/example/xiazhituo/ecase/MainActivity.java
@@ -2,9 +2,31 @@ package com.example.xiazhituo.ecase;
 
 import com.dd.CircularProgressButton;
 import com.example.xiazhituo.ecase.CircleProgressView;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+import android.app.AlertDialog;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.ScanResult;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.WindowManager;
 import android.widget.Button;
 
@@ -18,11 +40,52 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.animation.ValueAnimator;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+import android.bluetooth.le.ScanCallback;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    CircularProgressButton getWeightBut;
-    CircleProgressView unlockBtn;
-    CircleProgressView mCircleView;
+    //CircularProgressButton getWeightBut;
+    CircleProgressView mUnlockCircleView;
+    CircleProgressView mWeightCircleView;
+
+    /* For Bluetooth */
+    public BluetoothDevice mDevice;
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    // stop scan after 10 second
+    private static final long SCAN_PERIOD = 100000;
+    private String TAG = MainActivity.class.getSimpleName();
+
+    /* For ble service and char */
+    private String mDeviceName;
+    private String mDeviceAddress;
+    private ExpandableListView mGattServicesList;
+    public static BluetoothGatt mBluetoothGatt;
+    private BluetoothLeService mBluetoothLeService;
+    private BluetoothGattService mGattService;
+    private ArrayList<ArrayList<BluetoothGattCharacteristic>> mGattCharacteristics = new ArrayList<ArrayList<BluetoothGattCharacteristic>>();
+    private BluetoothGattCharacteristic mNftCharacteristic;
+    private BluetoothGattCharacteristic mWriteCharacteristic;
+
+
+    private String ECASE_SERVICE_UUID = "08590F7A-DB05-467E-8757-72F6FAEB13D4";
+    private String ECASE_NTF_CHARACTERISTIC_UUID = "08590F7E-DB05-467E-8757-72F6FAEB13D4";
+    private String ECASE_WRITE_CHARACTERISTIC_UUID = "08590F7F-DB05-467E-8757-72F6FAEB13D4";
+    private boolean mConnected = false;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,12 +96,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Snackbar.make(view, "Loading Map", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
                 Intent intent = new Intent(MainActivity.this, LocateMapActivity.class);
                 startActivity(intent);
             }
@@ -48,33 +109,69 @@ public class MainActivity extends AppCompatActivity {
         ble_fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Snackbar.make(view, "Loading Map", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
-
-
+                scanLeDevice(true);
             }
         });
 
-        unlockBtn = (CircleProgressView) findViewById(R.id.unlockBtn);
-        unlockBtn.setOnClickListener(new View.OnClickListener() {
+
+        mUnlockCircleView = (CircleProgressView) findViewById(R.id.unlockBtn);
+        mUnlockCircleView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //unlockBtn.setProgress(0);
-                //Snackbar.make(v, "Unlock command sent!", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
+				sendBleUnlock();
             }
         });
 
-        mCircleView = (CircleProgressView) findViewById(R.id.weightCircleProgress);
-        mCircleView.setOnClickListener(new View.OnClickListener() {
+        mWeightCircleView = (CircleProgressView) findViewById(R.id.weightCircleProgress);
+        mWeightCircleView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int progress = 20;
                 String weightHint = progress + " Kg";
-                mCircleView.setmTxtHint1(weightHint);
-                mCircleView.setProgress(progress);
+                mWeightCircleView.setmTxtHint1(weightHint);
+                mWeightCircleView.setProgress(progress);
             }
         });
+
+        /** For Bluetooth **/
+
+        mHandler = new Handler();
+
+        // 检查当前手机是否支持ble 蓝牙,如果不支持退出程序
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "ble_not_supported", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        // 初始化 Bluetooth adapter, 通过蓝牙管理器得到一个参考蓝牙适配器(API必须在以上android4.3或以上和版本)
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // 检查设备上是否支持蓝牙
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "error_bluetooth_not_supported", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        /* For Ble service */
+        /*
+        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+
+        boolean bll =  MainActivity.this.getApplicationContext().bindService(gattServiceIntent,
+                mServiceConnection, BIND_AUTO_CREATE);
+
+        if (bll) {
+            System.out.println("---------------");
+        } else {
+            System.out.println("===============");
+        }
+        */
+
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
     }
 
     @Override
@@ -112,4 +209,264 @@ public class MainActivity extends AppCompatActivity {
         });
         widthAnimation.start();
     }
+
+    /* For Ble */
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+
+
+                    mBluetoothAdapter.getBluetoothLeScanner().stopScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.getBluetoothLeScanner().startScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.getBluetoothLeScanner().stopScan(mLeScanCallback);
+        }
+    }
+
+    public ScanCallback mLeScanCallback = new ScanCallback() {
+
+        @Override
+
+        public void onScanResult(int callbackType, final ScanResult scanResult) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    String devName = scanResult.getDevice().getName();
+                    Log.d(TAG, "run:devName " + devName);
+
+                    if (devName != null) {
+
+                        if ((devName.contains("Luggage"))) {
+                            mDevice = scanResult.getDevice();
+                            mDeviceAddress = scanResult.getDevice().getAddress();
+                            Toast.makeText(getBaseContext(), devName, Toast.LENGTH_SHORT).show();
+
+                            Intent gattServiceIntent = new Intent(getBaseContext(), BluetoothLeService.class);
+
+                            boolean bll = MainActivity.this.getApplicationContext().bindService(gattServiceIntent,
+                                    mServiceConnection, BIND_AUTO_CREATE);
+
+                            if (bll) {
+                                System.out.println("Bond connect OK");
+                            } else {
+                                System.out.println("Bond connect FAIL");
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    };
+
+
+    /**********
+     * For Ble service
+     */
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName,
+                                       IBinder service) {
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up
+            // initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+            System.out.println("on Service connected 0000000");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
+                mConnected = true;
+                //updateConnectionState(R.string.connected);
+                //invalidateOptionsMenu();
+                //displayGattServices(mBluetoothLeService.getSupportedGattServices());
+            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                mConnected = false;
+                //updateConnectionState(R.string.disconnected);
+                //invalidateOptionsMenu();
+                //clearUI();
+            } else if (BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)) {
+                displayGattServices(mBluetoothLeService.getSupportedGattServices());
+            } else if (BluetoothLeService.ACTION_DATA_AVAILABLE.equals(action)) {
+                //displayData(intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
+                System.out.println("Get Data from device");
+                StringBuffer sb = new StringBuffer();
+
+                sb.append(intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
+                Log.d(TAG, "Gooooot Data: "+sb.toString());
+            }
+        }
+    };
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
+        intentFilter.addAction(BluetoothLeService.EXTRA_DATA);
+        return intentFilter;
+    }
+
+    // Demonstrates how to iterate through the supported GATT
+    // Services/Characteristics.
+    // In this sample, we populate the data structure that is bound to the
+    // ExpandableListView
+    // on the UI.
+    private void displayGattServices(List<BluetoothGattService> gattServices) {
+        if (gattServices == null)
+            return;
+        String serviceUuid = null;
+        String characteristicUuid = null;
+        // Loops through available GATT Services.
+        for (BluetoothGattService gattService : gattServices) {
+            serviceUuid = gattService.getUuid().toString();
+            if (serviceUuid.equalsIgnoreCase(ECASE_SERVICE_UUID))
+            {
+                Log.d(TAG, "displayGattServices: " + serviceUuid);
+
+                List<BluetoothGattCharacteristic> gattCharacteristics = gattService.getCharacteristics();
+                for (BluetoothGattCharacteristic gattCharacteristic : gattCharacteristics) {
+                    characteristicUuid = gattCharacteristic.getUuid().toString();
+                    if (characteristicUuid.equalsIgnoreCase(ECASE_NTF_CHARACTERISTIC_UUID)) {
+                        Log.e("console", "NTF Characteristic: " + characteristicUuid);
+
+                        mBluetoothLeService.setCharacteristicNotification(gattCharacteristic, true);
+                        mNftCharacteristic = gattCharacteristic;
+                        //mBluetoothLeService.readCharacteristic(gattCharacteristic);
+
+                    }
+
+                    if (characteristicUuid.equalsIgnoreCase(ECASE_WRITE_CHARACTERISTIC_UUID)) {
+                        mWriteCharacteristic = gattCharacteristic;
+                        System.out.println("Write Characteristic: " + characteristicUuid);
+                    }
+                }
+            }
+        }
+    }
+
+    /****
+     * common
+     ****/
+    private static byte[] reverseBytes(byte[] a) {
+        int len = a.length;
+        byte[] b = new byte[len];
+        for (int k = 0; k < len; k++) {
+            b[k] = a[a.length - 1 - k];
+        }
+        return b;
+    }
+
+    // byte转十六进制字符串
+    public static String bytes2HexString(byte[] bytes) {
+        String ret = "";
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(bytes[i] & 0xFF);
+            if (hex.length() == 1) {
+                hex = '0' + hex;
+            }
+            ret += hex.toUpperCase();
+        }
+        return ret;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.xiazhituo.ecase/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.xiazhituo.ecase/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            Log.d(TAG, "Connect request result=" + result);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mGattUpdateReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+    }
+	
+	public void sendBleUnlock()
+	{
+		String unlockCmd = "AT+LOCKOFF\r";
+		byte[] byteCmd = unlockCmd.getBytes(Charset.forName("US-ASCII"));
+        System.out.println("Write Data Lenght: " + byteCmd.length);
+        if (mWriteCharacteristic != null){
+            mBluetoothLeService.writeCharacteristic(mWriteCharacteristic, byteCmd);
+        } else {
+            System.out.println("Write Unlock failed ");
+        }
+
+	}
 }
